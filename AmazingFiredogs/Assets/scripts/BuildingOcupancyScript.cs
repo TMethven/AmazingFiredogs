592bf194f392eed733e5d33a58744bf495e38eb5
Tom Methven <t.methven@hw.ac.uk>
2017-11-11 20:45:23 +0000

    Added basic block dropping mechanics!
    In the MainDropScene scene, there is now a dropping block test. This is modular so you can just drop in a 'building' prefab and it'll drop room blocks onto the building every n seconds. Makes very scraggly buildings at the moment! Might need to be cleverer about selecting drop place!

--- AmazingFiredogs/Assets/scripts/BuildingOcupancyScript.cs
+++ AmazingFiredogs/Assets/scripts/BuildingOcupancyScript.cs
@@ -2,29 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BuildingOcupancyScript : MonoBehaviour {
+public class BuildingOcupancyScript : MonoBehaviour
+{
+    public float maxTimeToNextBlock = 1f;
+    public GameObject[] piecePrefab;
 
-    public const uint GridWidth = 4;
-    public const uint GridHeight = 100;
+    public const int GridWidth = 8;
+    public const int GridHeight = 100;
 
     public bool[,] OcupancyGrid;
     public GameObject[] HighestStairCase;
     public int[] HighestStairCaseHeight;
 
-    private int currentHeight = 0; 
+    private int currentHeight = 0;
+
+    private float timeSinceLastBlock = 0f; 
 
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
         OcupancyGrid = new bool[GridHeight, GridWidth];
         HighestStairCase = new GameObject[GridWidth];
         HighestStairCaseHeight = new int[GridWidth];
+
+        //Always start with a lobby piece!
+        addPiece(PieceControllerScript.PieceType.LOBBY);
     }
 
     public int checkDropHeight(int leftSideOffset, bool[,] blockOcupancyGrid)
     {
         int blockHeight = blockOcupancyGrid.GetLength(0);
         int blockWidth = blockOcupancyGrid.GetLength(1);
-        bool colides = false;
+            bool colides = false;
         int returnHeight = 0;
         for( int heightIndex = currentHeight; heightIndex >= 0; --heightIndex)
         {
@@ -68,7 +77,105 @@ public class BuildingOcupancyScript : MonoBehaviour {
 
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        timeSinceLastBlock += Time.deltaTime;
+
+        if(timeSinceLastBlock > maxTimeToNextBlock)
+        {
+            timeSinceLastBlock = 0f;
+            addPiece(getRandomPiece());
+        }
 	}
+
+    //Do a nicer random, based roughly on Tetris!
+    private int pieceIndex = 0;
+    private PieceControllerScript.PieceType[] pieceList;
+    private PieceControllerScript.PieceType getRandomPiece()
+    {
+        if (pieceList == null || pieceIndex == pieceList.Length)
+        {
+            pieceList = new PieceControllerScript.PieceType[PieceControllerScript.NumberOfPieceTypes * 2];
+
+            //Populate list...
+            for (int i = 0; i < pieceList.Length; i++)
+                pieceList[i] = (PieceControllerScript.PieceType)((i % PieceControllerScript.NumberOfPieceTypes) + 1);
+
+            //Shuffle list...
+            for (int i = 0; i < pieceList.Length; i++)
+            {
+                PieceControllerScript.PieceType tempPiece;
+                int randomIndex = Random.Range(i, pieceList.Length);
+                tempPiece = pieceList[i];
+                pieceList[i] = pieceList[randomIndex];
+                pieceList[randomIndex] = tempPiece;
+            }
+
+            pieceIndex = 0;
+        }
+
+        return pieceList[pieceIndex++];
+    }
+
+    private void addPiece(PieceControllerScript.PieceType piece)
+    {
+        GameObject tempGO;
+        PieceControllerScript tempPC;
+        int offsetFromLeft = 0;
+        int height;
+
+        if(piece == PieceControllerScript.PieceType.LOBBY)
+        {
+            offsetFromLeft = 0;
+            tempGO = Instantiate(piecePrefab[0]);
+            tempPC = tempGO.GetComponent<PieceControllerScript>();
+            tempPC.initPiece(this.gameObject, PieceControllerScript.PieceType.LOBBY, offsetFromLeft);
+
+            height = 0;
+        }
+        else if(piece == PieceControllerScript.PieceType.I)
+        {
+            offsetFromLeft = Random.Range(0, GridWidth - 3);
+            tempGO = Instantiate(piecePrefab[1]);
+            tempPC = tempGO.GetComponent<PieceControllerScript>();
+            tempPC.initPiece(this.gameObject, PieceControllerScript.PieceType.I, offsetFromLeft);
+
+            height = checkDropHeight(offsetFromLeft, tempPC.getPieceOccupancy());
+        }
+        else if (piece == PieceControllerScript.PieceType.S)
+        {
+            offsetFromLeft = Random.Range(0, GridWidth - 1);
+            tempGO = Instantiate(piecePrefab[2]);
+            height = 0;
+            tempPC = tempGO.GetComponent<PieceControllerScript>();
+            tempPC.initPiece(this.gameObject, PieceControllerScript.PieceType.S, offsetFromLeft);
+
+            height = checkDropHeight(offsetFromLeft, tempPC.getPieceOccupancy());
+        }
+        else if (piece == PieceControllerScript.PieceType.L)
+        {
+            offsetFromLeft = Random.Range(0, GridWidth - 1);
+            tempGO = Instantiate(piecePrefab[3]);
+            height = 0;
+            tempPC = tempGO.GetComponent<PieceControllerScript>();
+            tempPC.initPiece(this.gameObject, PieceControllerScript.PieceType.L, offsetFromLeft);
+
+            height = checkDropHeight(offsetFromLeft, tempPC.getPieceOccupancy());
+        }
+        else
+        {
+            offsetFromLeft = Random.Range(0, GridWidth - 2);
+            tempGO = Instantiate(piecePrefab[4]);
+            height = 0;
+            tempPC = tempGO.GetComponent<PieceControllerScript>();
+            tempPC.initPiece(this.gameObject, PieceControllerScript.PieceType.T, offsetFromLeft);
+
+            height = checkDropHeight(offsetFromLeft, tempPC.getPieceOccupancy());
+        }
+
+        addBlockAtHeight(height, offsetFromLeft, tempPC.getPieceOccupancy());
+        tempPC.setHeightAndPosition(height);
+
+        Debug.Log("Piece Added!");
+    }
 }
---
+++ AmazingFiredogs/Assets/scripts/PieceControllerScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceControllerScript : MonoBehaviour
+{
+    public enum PieceType { LOBBY, I, T, S, L};
+    public static int NumberOfPieceTypes = 4;
+
+    public PieceType pType; 
+
+    public int offsetFromLeft = 0, offsetFromBottom = 0;
+
+    private float unitSize = 1f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void initPiece(GameObject parent, PieceType piece, int offsetFromLeft)
+    {
+        this.transform.SetParent(parent.transform);
+
+        this.offsetFromLeft = offsetFromLeft;
+    }
+
+    public void setHeightAndPosition(int height)
+    {
+        offsetFromBottom = height;
+
+        this.transform.localPosition = new Vector3(offsetFromLeft * unitSize, offsetFromBottom * unitSize);
+    }
+
+    public bool[,] getPieceOccupancy()
+    {
+        if (pType == PieceType.LOBBY)
+            return LobbyPiece;
+        else if (pType == PieceType.I)
+            return IPiece;
+        else if (pType == PieceType.L)
+            return LPiece;
+        else if (pType == PieceType.S)
+            return SPiece;
+        else
+            return TPiece;
+    }
+
+    private bool[,] LobbyPiece = new bool[,]
+    {
+        { true, true, true, true, true, true, true, true }
+    };
+
+    private bool[,] IPiece = new bool[,]
+    {
+        { true, true, true, true }
+    };
+
+    private bool[,] TPiece = new bool[,]
+    {
+        { true, true, true },
+        { false, true, false }
+    };
+
+    private bool[,] SPiece = new bool[,]
+    {
+        { true, false },
+        { true, true },
+        { false, true }
+    };
+
+    private bool[,] LPiece = new bool[,]
+    {
+        { true, false },
+        { true, false },
+        { true, true }
+    };
+}
