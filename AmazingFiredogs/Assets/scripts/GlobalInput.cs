efb65fb3bdc72dde7c0a790c1516378cd84e03e0
Tom Methven <t.methven@hw.ac.uk>
2017-11-11 13:15:52 +0000

    Adding Global Controller Stuff and First Scene

---
+++ AmazingFiredogs/Assets/scripts/GlobalInput.cs
@@ -0,0 +1,11 @@
+﻿public class GlobalInput
+{
+    public static string[] Vertical = { "Keyboard0Vertical", "Keyboard1Vertical", "Joy0Vertical", "Joy1Vertical", "Joy2Vertical", "Joy3Vertical" };
+    public static string[] Horizontal = { "Keyboard0Horizontal", "Keyboard1Horizontal", "Joy0Horizontal", "Joy1Horizontal", "Joy2Horizontal", "Joy3Horizontal" };
+    public static string[] Jets = { "Keyboard0Jets", "Keyboard1Jets", "Joy0Jets", "Joy1Jets", "Joy2Jets", "Joy3Jets" };
+    public static string[] Fire = { "Keyboard0Fire", "Keyboard1Fire", "Joy0Fire", "Joy1Fire", "Joy2Fire", "Joy3Fire" };
+    public static string[] Tether = { "Keyboard0Tether", "Keyboard1Tether", "Joy0Tether", "Joy1Tether", "Joy2Tether", "Joy3Tether" };
+    public static string[] Shield = { "Keyboard0Shield", "Keyboard1Shield", "Joy0Shield", "Joy1Shield", "Joy2Shield", "Joy3Shield" };
+    public static string[] Pause = { "Keyboard0Pause", "Keyboard1Pause", "Joy0Pause", "Joy1Pause", "Joy2Pause", "Joy3Pause" };
+    public static string[] Back = { "Keyboard0Back", "Keyboard1Back", "Joy0Back", "Joy1Back", "Joy2Back", "Joy3Back" };
+}
