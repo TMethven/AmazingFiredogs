2fcd3bdd457d90c7cf81adcb2f91535985f30f5e
John Turner <git@norgg.org>
2017-11-12 11:51:42 +0000

    Rescuable human.

---
+++ AmazingFiredogs/Assets/scripts/Rescuable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rescuable : MonoBehaviour {
+
+	void Start() {
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		RescueZone rescue = other.GetComponent<RescueZone>();
+		if (rescue) {
+			Destroy(gameObject);
+		}
+	}
+}
