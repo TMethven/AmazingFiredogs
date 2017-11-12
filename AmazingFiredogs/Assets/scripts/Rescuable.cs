cb2568f210eba5a1a64735de344dc995614b743f
John Turner <git@norgg.org>
2017-11-12 15:03:00 +0000

    Cooking.

--- AmazingFiredogs/Assets/scripts/Rescuable.cs
+++ AmazingFiredogs/Assets/scripts/Rescuable.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Rescuable : MonoBehaviour {
 	public int BuildingNum;
+	FireControllerScript building1;
+	FireControllerScript building2;
+	public GameObject cookedPrefab;
+
+	int burned = 0;
 
 	void Start() {
 		if (transform.position.x > 0) {
@@ -11,6 +16,19 @@ public class Rescuable : MonoBehaviour {
 		} else {
 			BuildingNum = 1;
 		}
+
+		building1 = GameObject.Find("P1Building").GetComponent<FireControllerScript>();
+		building2 = GameObject.Find("P2Building").GetComponent<FireControllerScript>();
+	}
+
+	void Update() {
+		int fireLevel = building1.checkFireLevel(transform.position) + building2.checkFireLevel(transform.position);
+		burned += fireLevel;
+		if (burned > 1000) {
+			GameObject.Instantiate(cookedPrefab, transform.position, transform.rotation);
+			GameObject.Destroy(gameObject);
+		}
+
 	}
 	
 	void OnTriggerEnter2D(Collider2D other) {
