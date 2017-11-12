6b467da299e5fcb0595e67ee70d6e954c255d1d8
John Turner <git@norgg.org>
2017-11-12 14:55:17 +0000

    Camera follow.

---
+++ AmazingFiredogs/Assets/scripts/CamFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour {
+
+	public Transform target;
+
+	void Start() {
+		
+	}
+	
+	void FixedUpdate() {
+		transform.position = Vector3.Lerp(transform.position, target.position + (Vector3.up * 4) - Vector3.forward * 10, 0.1f);
+	}
+}
