e8b5b2107193ec04146cecfe0fe8550e356e88ec
John Turner <git@norgg.org>
2017-11-25 16:37:14 +0000

    Speech bubbles.

---
+++ AmazingFiredogs/Assets/scripts/SpeechBubble.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechBubble : MonoBehaviour {
+
+	void Start() {
+		SpriteRenderer personSprite = transform.Find("Human").GetComponent<SpriteRenderer>();
+		PlayerMove move = GetComponentInParent<PlayerMove>();
+		Color tint;
+		if (move.PlayerNum == 1) {
+			tint = GlobalInput.DogColours[(int)GlobalInput.Player1DogType];
+		} else {
+			tint = GlobalInput.DogColours[(int)GlobalInput.Player2DogType];
+		}
+		personSprite.color = tint;
+
+		Destroy(gameObject, 5);
+	}
+}
